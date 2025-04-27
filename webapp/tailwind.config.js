a65e23c464b0f6ea133b539a953b50166ba0434d
korchagingeorge <yaro010709@gmail.com>
2025-04-27 11:42:03 +0300

    Create tailwind.config.js

---
+++ webapp/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0d0d0d',
+        gold: '#d4af37',
+        text: '#e6e6e6'
+      },
+      borderRadius: {
+        xl: '1rem',
+        '2xl': '1.5rem'
+      }
+    }
+  },
+  plugins: []
+};
