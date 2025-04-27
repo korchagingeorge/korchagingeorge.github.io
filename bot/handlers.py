09d6c20275c939c6c101657983c957ae3146ddcb
korchagingeorge <yaro010709@gmail.com>
2025-04-27 11:45:38 +0300

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,34 +2,44 @@ from aiogram import types, Dispatcher
 from aiogram.types import WebAppInfo
 from db import SessionLocal
 from models import User
-from telegram import Bot as TgBot
+import json
+
+GITHUB_APP_URL = "https://korchagingeorge.github.io"
 
 async def start_command(message: types.Message):
     session = SessionLocal()
-    # parse referral
     args = message.get_args()
     user = session.query(User).filter_by(telegram_id=message.from_user.id).first()
     if not user:
         user = User(telegram_id=message.from_user.id, username=message.from_user.username)
+        # Обработка реферала
         if args:
-            ref_id = int(args)
-            ref_user = session.query(User).get(ref_id)
-            if ref_user:
-                user.referred_by = ref_id
-                ref_user.tickets += 1
-                session.add(ref_user)
+            try:
+                ref_id = int(args)
+                ref_user = session.query(User).get(ref_id)
+                if ref_user:
+                    user.referred_by = ref_id
+                    ref_user.tickets += 1
+                    session.add(ref_user)
+            except ValueError:
+                pass
         session.add(user)
         session.commit()
-    await message.answer("Добро пожаловать!", reply_markup=types.ReplyKeyboardMarkup(
-        keyboard=[[types.KeyboardButton(text="Открыть приложение", web_app=WebAppInfo(url="YOUR_GITHUB_PAGES_URL"))]], resize_keyboard=True)
+    # Кнопка для открытия Web App
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(
+        text="🌌 Открыть Ton Space", web_app=WebAppInfo(url=GITHUB_APP_URL)
+    ))
+    await message.answer("Добро пожаловать в Ton Space!", reply_markup=keyboard)
     session.close()
 
-async def webapp_data_handler(query: types.CallbackQuery):
-    data = query.data  # JSON с указанием вкладки и действий
-    # обработать действия и вернуть данные в WebApp через query.answer
-    await query.answer()
+async def callback_handler(query: types.CallbackQuery):
+    # Обработка данных из Web App (через query.data)
+    data = json.loads(query.data)
+    # TODO: разбор data и выполнение действий
+    await query.answer('Готово')
 
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
-    dp.register_callback_query_handler(webapp_data_handler, lambda c: True)
+    dp.register_callback_query_handler(callback_handler)
